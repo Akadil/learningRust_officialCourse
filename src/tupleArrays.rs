289a870b61c5929dc67bcae0471685c7281aa0c8
Akadil Kalimoldayev <akadilkalimoldayev@Akadils-MacBook-Air.local>
2024-08-09 21:16:37 +0500

    Day1 afternoon, 1/3

---
+++ src/tupleArrays.rs
@@ -0,0 +1,45 @@
+// TODO: remove this when you're done with your implementation.
+// #![allow(unused_variables, dead_code)]
+
+fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
+    let mut transposed = matrix;
+
+    for i in 0..3 {
+        for j in 0..3 {
+            transposed[i][j] = matrix[j][i];
+        }
+    }
+    transposed
+}
+
+// #[test]
+fn test_transpose() {
+    let matrix = [
+        [101, 102, 103], //
+        [201, 202, 203],
+        [301, 302, 303],
+    ];
+    let transposed = transpose(matrix);
+    assert_eq!(
+        transposed,
+        [
+            [101, 201, 301], //
+            [102, 202, 302],
+            [103, 203, 303],
+        ]
+    );
+    println!("Test passed");
+}
+
+fn main() {
+    let matrix = [
+        [101, 102, 103], // <-- the comment makes rustfmt add a newline
+        [201, 202, 203],
+        [301, 302, 303],
+    ];
+
+    test_transpose();
+    println!("matrix: {:?}", matrix);
+    let transposed = transpose(matrix);
+    println!("transposed: {:#?}", transposed);
+}
